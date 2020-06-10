12996d08818f8b2234a3eeb7d1f1c356ce9331ef
MarvinJWendt <git@marvinjwendt.com>
2020-06-10 04:05:56 +0200

    Update docs

--- rgtm.go
+++ rgtm.go
@@ -3,6 +3,13 @@ package rgtm
 import "regexp"
 
 // StringToMap returns a map with the regex group names and their values.
+// Regex: (?P<first>\\w+) (?P<middle>.+) (?P<last>\\w+)
+// String: "Marvin J Wendt"
+// Returns:
+// 	m[""] = "Marvin J Wendt"
+//  m["first"] = "Marvin"
+//  m["middle"] = "J"
+//  m["last"] = "Wendt"
 func StringToMap(r *regexp.Regexp, s string) (map[string]string, error) {
 	names := r.SubexpNames()
 	result := r.FindAllStringSubmatch(s, -1)
